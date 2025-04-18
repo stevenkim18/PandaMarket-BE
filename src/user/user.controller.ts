045988d47f9202596700d561d9472b14c3bb416d
stevenkim18 <11882@naver.com>
2025-04-19 01:03:44 +0900

    회원가입, 로그인 api 구현

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { UserService } from './user.service';
-import { User } from 'generated/prisma';
 import { CreateUserDto } from './dto/CreateUserDto';
+import { LoginUserDto } from './dto/LoginUserDto';
 
 @Controller('user')
 export class UserController {
@@ -10,5 +10,11 @@ export class UserController {
   @Post('/signup')
   createUser(@Body() user: CreateUserDto) {
     return this.userService.createUser(user);
+  }
+
+  @Post('/login')
+  @HttpCode(200)
+  login(@Body() user: LoginUserDto) {
+    return this.userService.login(user);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,19 +1,53 @@
-import { Injectable } from '@nestjs/common';
-import { User } from '@prisma/client';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateUserDto } from './dto/CreateUserDto';
+import { LoginUserDto } from './dto/LoginUserDto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UserService {
   constructor(private readonly prisma: PrismaService) {}
 
   async createUser(user: CreateUserDto) {
+    const hashedPassword = await bcrypt.hash(user.password, 10);
+
+    const existingUser = await this.prisma.user.findUnique({
+      where: { email: user.email },
+    });
+
+    if (existingUser) {
+      throw new BadRequestException('이미 존재하는 이메일입니다.');
+    }
+
     return this.prisma.user.create({
       data: {
         email: user.email,
         nickname: user.nickname,
-        password: user.password,
+        password: hashedPassword,
       },
     });
+  }
+
+  async login(user: LoginUserDto) {
+    const existingUser = await this.prisma.user.findUnique({
+      where: { email: user.email },
+    });
+
+    if (!existingUser) {
+      throw new BadRequestException('존재하지 않는 이메일입니다.');
+    }
+
+    const isPasswordValid = await bcrypt.compare(
+      user.password,
+      existingUser.password,
+    );
+
+    if (!isPasswordValid) {
+      throw new BadRequestException('비밀번호가 일치하지 않습니다.');
+    }
+
+    // password를 제외한 유저 정보
+    const { password, ...userInfo } = existingUser;
+    return userInfo;
   }
 }
